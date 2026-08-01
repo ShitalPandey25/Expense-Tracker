eb7c929484738393ebf5eb5efd2321f4f9a5dcd8
Shital Pandey <shitalp.mca25@sbjit.edu.in>
2026-08-02 00:17:48 +0530

    Fix API URL

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -28,7 +28,7 @@ function Login() {
 
     try {
       const response = await fetch(
-       `${import.meta.env.VITE_API_URL}api/auth/login`,
+       `${import.meta.env.VITE_API_URL}/api/auth/login`,
         {
           method: "POST",
           headers: {
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -58,7 +58,7 @@ function Register(){
     try{
       const response = await fetch(
 
-      `${import.meta.env.VITE_API_URL}api/auth/register`,
+      `${import.meta.env.VITE_API_URL}/api/auth/register`,
 
         {
 
